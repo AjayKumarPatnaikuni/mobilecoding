ba0e0bc6c7130b3f33ad3c833ca5322c91bc8ee0
Amarendar M <musham.amarendar@gmail.com>
2018-05-18 05:40:23 +0530

    Create BatteryHealthCheck.sh

---
+++ BatteryHealthCheck.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+while :
+	do
+		a=`termux-battery-status | grep percentage | awk '{print $2}' | cut -d, -f1`
+        k=`termux-battery-status | grep status | awk '{print $2}' | cut -d'"' -f2`
+        b=`echo $a-1|bc -l`
+		
+		function charging {  termux-tts-speak "your battery fully charged  and it is $a percentage now" && termux-vibrate -d 2000 -f ; }
+		function dis_charging { termux-tts-speak "your battery is going to die and it is bloody $a percentage now" && termux-vibrate -d 2000 -f ; }
+		
+		if [ "$k" == "FULL" ] ; then
+                charging ; sleep 1 ;
+        elif [ "$k" == "CHARGING" ] && [ $b -gt 98 ] ; then
+				charging ; sleep 2 ;
+		elif [ "$k" == "CHARGING" ] && [ $b -lt 98 ] ; then
+				sleep 5 ;
+		elif [ "$k" == "DISCHARGING" ] && [ $b -lt 35 ] ; then
+				dis_charging ; sleep 2 ;
+		elif [ "$k" == "DISCHARGING" ] && [ $b -gt 35 ] ; then
+				sleep 5 ;
+		fi
+		
+	done
+  
